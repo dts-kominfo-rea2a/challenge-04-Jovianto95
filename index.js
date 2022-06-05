0789873ab5c4b881d564037815895e6cddb240d5
Jovianto Trisila <mandevil41@gmail.com>
2022-06-05 18:24:24 +0700

    Done

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ const dates = [
   "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
 ];
 
+
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (dates, index) => {
+  let result = [];
+  for (let index = 0; index < dates.length; index++){
+    result.push(Date.parse(dates[index]) / 1000);
+  }
+  if (index != null){
+    return result[index].toString();
+  } else {
+    return result.sort().join("-").toString();
+  }
+}
 
 // ! JANGAN DIMODIFIKASI
 (() => {
